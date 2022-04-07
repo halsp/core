da8f89566c74d5e76df9402b4fc9a48906c32547
hal-wang <hi@hal.wang>
2022-04-07 16:24:53 +0800

    test: function middleware constructor

--- packages/inject/test/middleware-constructor.test.ts
+++ packages/inject/test/middleware-constructor.test.ts
@@ -29,3 +29,40 @@ test(`middleware constructor`, async () => {
   });
   expect(res.status).toBe(200);
 });
+
+test(`function middleware constructor`, async () => {
+  const res = await new TestStartup()
+    .useInject()
+    .add((ctx) => {
+      ctx.setHeader("h", "1");
+      return TestMiddleware;
+    })
+    .run();
+
+  expect(res.body).toEqual({
+    md: "md.service2.service1",
+    num: new Number(),
+  });
+  expect(res.status).toBe(200);
+  expect(res.getHeader("h")).toBe("1");
+});
+
+test(`async function middleware constructor`, async () => {
+  const res = await new TestStartup()
+    .useInject()
+    .add(async (ctx) => {
+      await new Promise<void>((resolve) => {
+        setTimeout(() => resolve(), 200);
+      });
+      ctx.setHeader("h", "1");
+      return TestMiddleware;
+    })
+    .run();
+
+  expect(res.body).toEqual({
+    md: "md.service2.service1",
+    num: new Number(),
+  });
+  expect(res.status).toBe(200);
+  expect(res.getHeader("h")).toBe("1");
+});
