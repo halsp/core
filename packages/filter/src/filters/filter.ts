8e987e3cffee5c834c7ebbdcf21e57d9b180a053
hal-wang <hi@hal.wang>
2022-04-29 09:33:14 +0800

    fix: remove Filter extends

--- packages/filter/src/filters/filter.ts
+++ packages/filter/src/filters/filter.ts
@@ -1,7 +1,7 @@
 import { ObjectConstructor } from "@sfajs/core";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Filter extends Object {}
+export interface Filter {}
 
 export type FilterItem<T extends Filter = Filter> = T | ObjectConstructor<T>;
 
