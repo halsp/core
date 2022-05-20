60821fe3b7ee32c3b5bfcd44b7739cb2b0595067
hal-wang <hi@hal.wang>
2022-05-21 00:44:15 +0800

    fix: cd root after runin

--- packages/testing/src/shell.ts
+++ packages/testing/src/shell.ts
@@ -3,10 +3,11 @@ import * as shell from "shelljs";
 export { shell };
 
 export async function runin(path: string, func: () => Promise<void> | void) {
+  const root = process.cwd();
   shell.cd(path);
   try {
     await func();
   } finally {
-    shell.cd(process.cwd());
+    shell.cd(root);
   }
 }
