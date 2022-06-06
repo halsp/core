0ca4e857570dee6ed8afa3ca50c349b3ad353fc2
hal-wang <hi@hal.wang>
2022-06-06 09:11:32 +0800

    fix: redefine err

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -60,77 +60,84 @@ declare module "@sfajs/cli" {
   }
 }
 
-Object.defineProperty(HttpContext.prototype, "actionMetadata", {
-  configurable: false,
-  enumerable: false,
-  get: function () {
-    const ctx = this as HttpContext;
-    if (ctx[CTX_CACHE_METADATA]) {
-      return ctx[CTX_CACHE_METADATA];
-    }
-
-    const cfg: RouterConfig = this.startup[STARTUP_ROUTER_CONFIG];
-    const mapParser = new MapParser(ctx, cfg);
-    if (mapParser.notFound) {
-      throw new NotFoundException({
-        message: `Can't find the path：${ctx.req.path}`,
-        path: ctx.req.path,
-      });
-    }
-    if (mapParser.methodNotAllowed) {
-      throw new MethodNotAllowedException({
-        message: `method not allowed：${ctx.req.method}`,
-        method: ctx.req.method,
-        path: ctx.req.path,
-      });
-    }
-    const mapItem = mapParser.mapItem;
-    ctx[CTX_CACHE_METADATA] = mapItem;
-    return mapItem;
-  },
-});
-
-Object.defineProperty(SfaRequest.prototype, "params", {
-  configurable: false,
-  enumerable: false,
-  get: function () {
-    const req = this as SfaRequest;
-    if (req[REQUEST_CACHE_PARAMS]) {
-      return req[REQUEST_CACHE_PARAMS];
-    }
-
-    const params: QueryDict = {};
-    const actionMetadata: MapItem = req.ctx.actionMetadata;
-    if (actionMetadata.path.includes("^")) {
-      const mapPathStrs = actionMetadata.url.split("/");
-      const reqPathStrs = req.path.split("/");
-      for (
-        let i = 0;
-        i < Math.min(mapPathStrs.length, reqPathStrs.length);
-        i++
-      ) {
-        const mapPathStr = mapPathStrs[i];
-        if (!mapPathStr.startsWith("^")) continue;
-        const reqPathStr = reqPathStrs[i];
-
-        const key = mapPathStr.substring(1, mapPathStr.length);
-        const value = decodeURIComponent(reqPathStr);
-
-        params[key] = value;
+if (!HttpContext.prototype.hasOwnProperty("actionMetadata")) {
+  Object.defineProperty(HttpContext.prototype, "actionMetadata", {
+    configurable: false,
+    enumerable: false,
+    get: function () {
+      const ctx = this as HttpContext;
+      if (ctx[CTX_CACHE_METADATA]) {
+        return ctx[CTX_CACHE_METADATA];
       }
-    }
-    req[REQUEST_CACHE_PARAMS] = params;
-    return params;
-  },
-});
-Object.defineProperty(SfaRequest.prototype, "param", {
-  configurable: false,
-  enumerable: false,
-  get: function () {
-    const req = this as SfaRequest;
-    return req.params;
-  },
-});
+
+      const cfg: RouterConfig = this.startup[STARTUP_ROUTER_CONFIG];
+      const mapParser = new MapParser(ctx, cfg);
+      if (mapParser.notFound) {
+        throw new NotFoundException({
+          message: `Can't find the path：${ctx.req.path}`,
+          path: ctx.req.path,
+        });
+      }
+      if (mapParser.methodNotAllowed) {
+        throw new MethodNotAllowedException({
+          message: `method not allowed：${ctx.req.method}`,
+          method: ctx.req.method,
+          path: ctx.req.path,
+        });
+      }
+      const mapItem = mapParser.mapItem;
+      ctx[CTX_CACHE_METADATA] = mapItem;
+      return mapItem;
+    },
+  });
+}
+
+if (!SfaRequest.prototype.hasOwnProperty("params")) {
+  Object.defineProperty(SfaRequest.prototype, "params", {
+    configurable: false,
+    enumerable: false,
+    get: function () {
+      const req = this as SfaRequest;
+      if (req[REQUEST_CACHE_PARAMS]) {
+        return req[REQUEST_CACHE_PARAMS];
+      }
+
+      const params: QueryDict = {};
+      const actionMetadata: MapItem = req.ctx.actionMetadata;
+      if (actionMetadata.path.includes("^")) {
+        const mapPathStrs = actionMetadata.url.split("/");
+        const reqPathStrs = req.path.split("/");
+        for (
+          let i = 0;
+          i < Math.min(mapPathStrs.length, reqPathStrs.length);
+          i++
+        ) {
+          const mapPathStr = mapPathStrs[i];
+          if (!mapPathStr.startsWith("^")) continue;
+          const reqPathStr = reqPathStrs[i];
+
+          const key = mapPathStr.substring(1, mapPathStr.length);
+          const value = decodeURIComponent(reqPathStr);
+
+          params[key] = value;
+        }
+      }
+      req[REQUEST_CACHE_PARAMS] = params;
+      return params;
+    },
+  });
+}
+
+if (!SfaRequest.prototype.hasOwnProperty("param")) {
+  Object.defineProperty(SfaRequest.prototype, "param", {
+    configurable: false,
+    enumerable: false,
+    get: function () {
+      const req = this as SfaRequest;
+      return req.params;
+    },
+  });
+}
 
 Startup.prototype.useRouter = function (): Startup {
   if (!!this[STARTUP_ROUTER_CONFIG]) {
