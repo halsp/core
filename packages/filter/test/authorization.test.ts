fdea4142e0917b27e43ef66d03640c4d994dbd64
hal-wang <hi@hal.wang>
2022-06-13 07:38:28 +0800

    feat: use router 2.x

--- packages/filter/test/authorization.test.ts
+++ packages/filter/test/authorization.test.ts
@@ -3,28 +3,31 @@ import "../src";
 
 function runTest(executing: boolean) {
   test(`authorization filter ${executing}`, async () => {
-    const res = await new TestStartup(
-      new SfaRequest().setMethod("get").setPath("auth").setBody({
-        executing,
-      })
-    )
-      .use(async (ctx, next) => {
-        ctx.setHeader("h1", 1);
-        await next();
-        ctx.setHeader("h2", 2);
-      })
-      .useFilter()
-      .useRouter({
-        dir: "test/actions",
-      })
-      .run();
+    process.chdir("test");
+    try {
+      const res = await new TestStartup(
+        new SfaRequest().setMethod("get").setPath("auth").setBody({
+          executing,
+        })
+      )
+        .use(async (ctx, next) => {
+          ctx.setHeader("h1", 1);
+          await next();
+          ctx.setHeader("h2", 2);
+        })
+        .useFilter()
+        .useRouter()
+        .run();
 
-    expect(res.getHeader(`h1`)).toBe("1");
-    expect(res.getHeader(`h2`)).toBe("2");
-    if (executing) {
-      expect(res.status).toBe(200);
-    } else {
-      expect(res.status).toBe(401);
+      expect(res.getHeader(`h1`)).toBe("1");
+      expect(res.getHeader(`h2`)).toBe("2");
+      if (executing) {
+        expect(res.status).toBe(200);
+      } else {
+        expect(res.status).toBe(401);
+      }
+    } finally {
+      process.chdir("..");
     }
   });
 }
