e423c3c33a00abf112fd0c41b2f02db2e62e6c5a
hal-wang <hi@hal.wang>
2022-06-21 23:00:58 +0800

    test: add record test

---
+++ packages/pipe/test/record.test.ts
@@ -0,0 +1,24 @@
+import { HookType, TestStartup } from "@sfajs/core";
+import { PIPE_RECORDS_METADATA } from "../src";
+import { expectBody, getTestRequest, TestMiddleware } from "./TestMiddleware";
+
+test("simple test", async function () {
+  let done = false;
+  const startup = new TestStartup(getTestRequest()).useInject();
+  startup.hook(HookType.BeforeInvoke, (ctx, md) => {
+    const metadata = Reflect.getMetadata(
+      PIPE_RECORDS_METADATA,
+      md.constructor.prototype
+    );
+    expect(metadata).not.toBeUndefined();
+    expect(Array.isArray(metadata)).toBeTruthy();
+    expect(metadata.length > 0).toBeTruthy();
+    done = true;
+  });
+  startup.add(TestMiddleware);
+
+  const res = await startup.run();
+  expect(done).toBeTruthy();
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual(expectBody);
+});
