7a7c80f9f4a57321feceadba6daaeb4ce2e68bd8
hal-wang <hi@hal.wang>
2022-06-28 23:12:04 +0800

    chore: update deps

--- packages/pipe/src/constant.ts
+++ packages/pipe/src/constant.ts
@@ -1,2 +1,2 @@
-export const PIPE_RECORDS_METADATA = "@sfajs/pipe/RecordsMetadata";
+export const PIPE_RECORDS_METADATA = "@sfajs/pipe/recordsMetadata";
 export const GLOBAL_PIPE_BAG = "@sfajs/pipe/globalPipeBag";
