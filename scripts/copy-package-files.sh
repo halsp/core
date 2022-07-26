886ff697be483176dc033960c6806d045759fed4
hal-wang <hi@hal.wang>
2022-07-26 14:42:54 +0800

    chore: remove package eslint files

--- scripts/copy-package-files.sh
+++ scripts/copy-package-files.sh
@@ -2,5 +2,7 @@ ts-node scripts/copy-package-file.ts base.gitignore .gitignore
 ts-node scripts/copy-package-file.ts jest.config.js
 ts-node scripts/copy-package-file.ts jest.setup.js
 ts-node scripts/copy-package-file.ts ../LICENSE LICENSE
+ts-node scripts/copy-package-file.ts ../.eslintignore .eslintignore
+ts-node scripts/copy-package-file.ts ../.eslintrc.js .eslintrc.js
 
 cp README.md packages/core/README.md
