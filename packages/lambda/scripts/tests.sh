39fbb01ce516004bae2afb14a4fdc40418351f1d
hal-wang <hi@hal.wang>
2022-07-26 14:06:02 +0800

    chore: remove package scripts

--- packages/lambda/scripts/tests.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-npm install
-npm run build
-
-cd demo && npm install && cd ..
-
-npm run lint
-npm run test
--- packages/static/scripts/tests.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-npm install
-npm run lint
-npm run build
-npm run test
