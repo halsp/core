8418c3c6e7558976edb583417c271297b3b7918a
hal-wang <hi@hal.wang>
2022-07-29 16:55:18 +0800

    feat(env): add use version

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,16 +1,25 @@
 import { Startup } from "@ipare/core";
 import { EnvOptions } from "./options";
 import { useSetup } from "./use-setup";
+import { useVersion } from "./use-version";
 
 declare module "@ipare/core" {
   interface Startup {
     useEnv(mode: string): this;
     useEnv(options?: EnvOptions): this;
+    useVersion(header?: string, cwd?: string): this;
   }
 }
 
 Startup.prototype.useEnv = function (options?: EnvOptions | string): Startup {
   return useSetup(this, options);
+};
+
+Startup.prototype.useVersion = function (
+  header = "version",
+  cwd = process.cwd()
+): Startup {
+  return useVersion(this, header, cwd);
 };
 
 export { EnvOptions, ModeConfigOptions, DotenvConfigOptions } from "./options";
---
+++ packages/env/src/use-version.ts
@@ -0,0 +1,16 @@
+import { Startup } from "@ipare/core";
+import path from "path";
+import * as fs from "fs";
+
+export function useVersion<T extends Startup>(
+  startup: T,
+  header: string,
+  cwd: string
+): T {
+  return startup.use(async (ctx, next) => {
+    const pkgStr = fs.readFileSync(path.join(cwd, "package.json"), "utf-8");
+    const version = JSON.parse(pkgStr).version ?? "0";
+    ctx.setHeader(header, version);
+    await next();
+  });
+}
