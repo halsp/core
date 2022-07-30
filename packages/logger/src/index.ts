c3c1158d77429dc5388a5444935116a538cbb816
hal-wang <hi@hal.wang>
2022-07-31 00:09:02 +0800

    feat(logger): quick use

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,12 +1,19 @@
 import { Startup } from "@ipare/core";
 import winston from "winston";
 import Transport from "winston-transport";
+import { FileTransportOptions } from "winston/lib/winston/transports";
 import { OPTIONS_IDENTITY } from "./constant";
 import { Options } from "./options";
 
 declare module "@ipare/core" {
   interface Startup {
     useLogger(options?: Options): this;
+    useConsoleLogger(options?: Omit<Options, "transports">): this;
+    useFileLogger(
+      options: Omit<Options, "transports"> & {
+        fileTransportOptions: FileTransportOptions;
+      }
+    ): this;
   }
 }
 
@@ -16,6 +23,20 @@ Startup.prototype.useLogger = function (options?: Options): Startup {
     () => winston.createLogger(options),
     options?.injectType
   );
+};
+
+Startup.prototype.useConsoleLogger = function (options: Options = {}): Startup {
+  options.transports = new winston.transports.Console();
+  return this.useLogger(options);
+};
+
+Startup.prototype.useFileLogger = function (
+  options: Options & { fileTransportOptions: FileTransportOptions }
+): Startup {
+  options.transports = new winston.transports.File(
+    options.fileTransportOptions
+  );
+  return this.useLogger(options);
 };
 
 export { winston, Transport };
---
+++ packages/logger/test/use.test.ts
@@ -0,0 +1,35 @@
+import { Middleware } from "@ipare/core";
+import { TestStartup } from "@ipare/core";
+import { Logger, winston } from "../src";
+import "../src";
+
+class TestMiddleware extends Middleware {
+  @Logger()
+  private readonly logger!: winston.Logger;
+
+  async invoke(): Promise<void> {
+    this.ctx.res["RESULT"] = this.logger.transports;
+  }
+}
+
+test("useConsoleLogger", async () => {
+  const res = await new TestStartup()
+    .useConsoleLogger()
+    .add(TestMiddleware)
+    .run();
+
+  expect(res["RESULT"][0] instanceof winston.transports.Console).toBeTruthy();
+});
+
+test("useFileLogger", async () => {
+  const res = await new TestStartup()
+    .useFileLogger({
+      fileTransportOptions: {
+        filename: "node_modules/test.logger.log",
+      },
+    })
+    .add(TestMiddleware)
+    .run();
+
+  expect(res["RESULT"][0] instanceof winston.transports.File).toBeTruthy();
+});
