9262f695e142d00987f270b6d8be2794575be47c
hal-wang <hi@hal.wang>
2022-07-30 12:43:03 +0800

    refactor(view): config hook use @ipare/common

--- packages/view/src/cli-config.ts
+++ packages/view/src/cli-config.ts
@@ -1,37 +1,13 @@
-import { isString } from "@ipare/core";
+import { tryAddCliAssets } from "@ipare/common";
 
 export const cliConfigHook = (config: any) => {
-  if (!config.build) {
-    config.build = {};
-  }
-  if (!config.build.assets) {
-    config.build.assets = [];
-  }
-
-  const assets = config.build.assets as any[];
-  if (!isAssetExist(assets, (ass) => ass.startsWith("views/"))) {
-    assets.push({
+  return tryAddCliAssets(
+    config,
+    (ass) => ass.startsWith("views/"),
+    {
       include: "views/*",
       root: "src",
-    });
-    assets.push("views/*");
-  }
-  return config;
+    },
+    "views/*"
+  );
 };
-
-function isAssetExist(
-  assets: any[],
-  compare: (asset: string) => boolean
-): boolean {
-  return assets.some((item) => {
-    if (isString(item)) {
-      return compare(item);
-    } else {
-      if (isString(item.include)) {
-        return compare(item.include);
-      } else {
-        return item.include.some((item: string) => compare(item));
-      }
-    }
-  });
-}
