ebad830431d19f3fc43473c602740f488effbd72
hal-wang <hi@hal.wang>
2022-07-30 12:50:48 +0800

    refactor(env): config hook use @ipare/common

--- packages/env/src/cli-config.ts
+++ packages/env/src/cli-config.ts
@@ -1,18 +1,11 @@
-import { isString } from "@ipare/core";
+import { getCliAssets, isCliAssetExist } from "@ipare/common";
 
 export const cliConfigHook = (config: any, { command }) => {
-  if (!config.build) {
-    config.build = {};
-  }
-  if (!config.build.assets) {
-    config.build.assets = [];
-  }
-
-  const assets = config.build.assets as any[];
-  if (!isAssetExist(assets, (ass) => ass == ".env")) {
+  const assets = getCliAssets(config);
+  if (!isCliAssetExist(assets, (ass) => ass == ".env")) {
     assets.push(".env");
   }
-  if (!isAssetExist(assets, (ass) => ass.startsWith(".env."))) {
+  if (!isCliAssetExist(assets, (ass) => ass.startsWith(".env."))) {
     if (command == "start") {
       assets.push(".env.*");
     } else {
@@ -24,20 +17,3 @@ export const cliConfigHook = (config: any, { command }) => {
   }
   return config;
 };
-
-function isAssetExist(
-  assets: any[],
-  compare: (asset: string) => boolean
-): boolean {
-  return assets.some((item) => {
-    if (isString(item)) {
-      return compare(item);
-    } else {
-      if (isString(item.include)) {
-        return compare(item.include);
-      } else {
-        return item.include.some((item: string) => compare(item));
-      }
-    }
-  });
-}
