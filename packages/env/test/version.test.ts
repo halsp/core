3d61d07af783856e85f4936445d8fc8f810e975f
hal-wang <hi@hal.wang>
2022-08-02 10:25:56 +0800

    test(env): add version test

--- packages/env/test/version.test.ts
+++ packages/env/test/version.test.ts
@@ -24,3 +24,40 @@ test("use version custom header", async () => {
   expect(res.getHeader("h1")).toBe("0");
   expect(res.getHeader("version")).toBeUndefined();
 });
+
+it("should find package.json up", async () => {
+  const current = process.cwd();
+  process.chdir("test");
+  try {
+    const res = await new TestStartup()
+      .useVersion()
+      .use((ctx) => {
+        ctx.ok(process.env);
+      })
+      .run();
+
+    const version = JSON.parse(
+      fs.readFileSync("../package.json", "utf-8")
+    ).version;
+    expect(res.getHeader("version")).toBe(version);
+  } finally {
+    process.chdir(current);
+  }
+});
+
+it("should not find package.json", async () => {
+  const current = process.cwd();
+  process.chdir("../../..");
+  try {
+    const res = await new TestStartup()
+      .useVersion()
+      .use((ctx) => {
+        ctx.ok(process.env);
+      })
+      .run();
+
+    expect(res.getHeader("version")).toBe("0");
+  } finally {
+    process.chdir(current);
+  }
+});
