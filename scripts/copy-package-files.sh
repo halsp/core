482d8b671f535da41ff152d0c92731b17b551b6b
hal-wang <hi@hal.wang>
2022-08-09 12:07:23 +0800

    chore: rename tsconfig.base.json

--- scripts/copy-package-files.sh
+++ scripts/copy-package-files.sh
@@ -1,7 +1,7 @@
 ts-node scripts/copy-package-file.ts base.gitignore .gitignore
 ts-node scripts/copy-package-file.ts jest.config.js
 ts-node scripts/copy-package-file.ts jest.setup.js
-ts-node scripts/copy-package-file.ts tsconfig.json
+ts-node scripts/copy-package-file.ts tsconfig.base.json tsconfig.json
 ts-node scripts/copy-package-file.ts ../LICENSE LICENSE
 ts-node scripts/copy-package-file.ts ../.eslintignore .eslintignore
 ts-node scripts/copy-package-file.ts ../.eslintrc.js .eslintrc.js
