7072a80f2c5af112f1be3d405190b3036fdf7cdf
hal-wang <hi@hal.wang>
2022-08-29 22:55:16 +0800

    feat(lambda): add removeDevDeps

--- packages/lambda/src/cli-config.ts
+++ packages/lambda/src/cli-config.ts
@@ -2,6 +2,11 @@ export const cliConfigHook = (config: any, { command }) => {
   if (!config.build) {
     config.build = {};
   }
-  config.build.copyPackage = command == "build";
+  if (typeof config.build.copyPackage == "undefined") {
+    config.build.copyPackage = command == "build";
+  }
+  if (typeof config.build.removeDevDeps == "undefined") {
+    config.build.removeDevDeps = command == "build";
+  }
   return config;
 };
