9ddec6f49200eba0ee6c020d36d92a4a44231fea
hal-wang <hi@hal.wang>
2023-01-13 16:55:38 +0800

    test: update script

--- scripts/init-docker.sh
+++ scripts/init-docker.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [[ -z $(docker ps -a -q -f "name=^ipare-test-nats$") ]];
+if [ -z $(docker ps -a -q -f "name=^ipare-test-nats$") ];
 then
   docker run -itd --name ipare-test-nats -p 6001:4222 nats:2-linux
 else
   docker start ipare-test-nats
 fi
 
-if [[ -z $(docker ps -a -q -f "name=^ipare-test-mqtt$") ]];
+if [ -z $(docker ps -a -q -f "name=^ipare-test-mqtt$") ];
 then
   docker run -itd --name ipare-test-mqtt -p 6002:1883 emqx:5
 else
   docker start ipare-test-mqtt
 fi
 
-if [[ -z $(docker ps -a -q -f "name=^ipare-test-redis$") ]];
+if [ -z $(docker ps -a -q -f "name=^ipare-test-redis$") ];
 then
   docker run -itd --name ipare-test-redis -p 6003:6379 redis:6
 else
