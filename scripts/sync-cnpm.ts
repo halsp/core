410b4d1c30a43e3085234b849de16ffa4c42f157
hal-wang <hi@hal.wang>
2023-06-08 23:11:18 +0800

    ci: add cnpm sync

---
+++ scripts/sync-cnpm.ts
@@ -0,0 +1,31 @@
+import { getPackages } from "./get-packages";
+
+const dynamicImport = new Function(
+  "specifier",
+  `return import(specifier);
+  `
+) as <T = any>(specifier: string) => Promise<T>;
+
+async function dynamicImportDefault<T = any>(specifier: string) {
+  const module = await dynamicImport(specifier);
+  return module.default as T;
+}
+
+type Fetch = typeof import("node-fetch").default;
+
+async function sync(fetch: Fetch, name: string) {
+  const url = `https://registry-direct.npmmirror.com/@halsp/${name}/sync?sync_upstream=true`;
+  const res = await fetch(url, {
+    method: "PUT",
+  });
+  const json: any = await res.json();
+  console.log(`sync ${name} ${json.ok ? "success" : "failed"}`);
+}
+
+(async () => {
+  const fetch: Fetch = await dynamicImportDefault("node-fetch");
+  const packages: string[] = getPackages();
+  for (const pkg of packages) {
+    await sync(fetch, pkg);
+  }
+})();
