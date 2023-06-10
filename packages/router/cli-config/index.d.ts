a62565e9d18c1e190feb8516e4f6a15fe33a186f
hal-wang <hi@hal.wang>
2023-06-10 17:23:59 +0800

    chore(router): move cli-config

--- packages/router/cli-config/index.d.ts
+++ packages/router/cli-config/index.d.ts
@@ -1,7 +1,5 @@
 import "@halsp/cli";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 declare module "@halsp/cli" {
   interface Configuration {
     routerActionsDir?: string;
