5e2f1066c4e25e51d364f8998ef4a1007be3882d
hal-wang <hi@hal.wang>
2024-04-10 23:55:42 +0800

    chore(core): update safeImport

--- packages/core/src/utils/module.ts
+++ packages/core/src/utils/module.ts
@@ -1,11 +1,16 @@
 import type {} from "@halsp/cli";
+import url from "url";
 
-export async function safeImport(name: string) {
+export async function safeImport<T = any>(name: string) {
   try {
     try {
-      return await import(name);
+      return (await import(name)) as T;
     } catch {
-      return _require(name);
+      try {
+        return (await import(url.pathToFileURL(name).toString())) as T;
+      } catch {
+        return _require(name) as T;
+      }
     }
   } catch {
     return null;
